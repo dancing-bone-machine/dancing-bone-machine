bcabc956146bfe9259da3987b66e13645b9d6d27
Rafael Vega <email.rafa@gmail.com>
2014-05-04 22:53:26 -0500

    deleted olkd qt template

--- templates/QT/src/Console.cpp
+++
@@ -1,8 +0,0 @@
-#include "Console.h"
-#include <iostream>
-
-DBM::Console::Console(QObject* parent): QObject(parent){}
-
-void DBM::Console::log(QString msg){
-   std::cout << qPrintable(msg) << std::endl;
-}
--- templates/QT/src/WebPage.cpp
+++
@@ -1,8 +0,0 @@
-#include "WebPage.h"
-
-DBM::WebPage::WebPage(QObject* parent): QWebPage(parent){}
-
-QString DBM::WebPage::userAgentForUrl(const QUrl &url ) const {
-   return QString("DancingBoneMachineQT");
-   (void)url;
-}
